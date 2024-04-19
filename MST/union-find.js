2a4c11dc094022e00eea0b92eb240712c3a484e2
Jeong seulho <wjdtmfgh@gmail.com>
2024-04-19 17:56:10 +0900

    add : Union Find Algorithm

---
+++ MST/union-find.js
@@ -0,0 +1,16 @@
+const getParent = (parent, x) => {
+  if (parent[x] === x) return x;
+  return (parent[x] = getParent(parent, parent[x]));
+};
+
+const unionParent = (parent, a, b) => {
+  const n1 = getParent(parent, a);
+  const n2 = getParent(parent, b);
+  n1 < n2 ? (parent[n2] = n1) : (parent[n1] = n2);
+};
+
+const isSameParent = (parent, a, b) => {
+  const n1 = getParent(parent, a);
+  const n2 = getParent(parent, b);
+  return n1 === n2;
+};
